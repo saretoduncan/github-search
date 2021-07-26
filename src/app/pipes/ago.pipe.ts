24b8fbad9172ad9b38bd76232456621cccb58354
saretoduncan <saretoduncan@gmail.com>
2021-07-26 17:13:15 +0300

    add time pipes

--- src/app/pipes/ago.pipe.ts
+++ src/app/pipes/ago.pipe.ts
@@ -1,12 +1,27 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'ago'
+  name: "ago",
 })
 export class AgoPipe implements PipeTransform {
+  transform(value: Date) {
+    var lastSeen: string = "";
+    let now: any = new Date().valueOf(); //current time
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    let secondsPassed = (now - value.getTime()) / 1000; //time passed in second
+
+    if (secondsPassed < 60) {
+      lastSeen = `${Math.floor(secondsPassed)} seconds ago `; // last seen in seconds
+    } else if (secondsPassed < 3600) {
+      lastSeen = `${Math.floor(secondsPassed / 60)} minutes ago`; //last seen in minutes
+    } else if (secondsPassed < 86400) {
+      lastSeen = `${Math.floor(secondsPassed / 3600)} hours ago`; //last seen in hours
+    } else if (secondsPassed < 2592000) {
+      lastSeen = `${Math.floor(secondsPassed / 86400)} days ago`; //last seen in days
+    } else {
+      lastSeen = `${Math.floor(secondsPassed / 2592000)} months ago`;
+    }
+
+    return lastSeen;
   }
-
 }

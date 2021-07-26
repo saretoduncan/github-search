521d3915df9cf72034b7445a614ce95c29682e53
saretoduncan <saretoduncan@gmail.com>
2021-07-26 13:35:09 +0300

    add nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { SearchCompComponent } from './search-comp/search-comp.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RepositoriesComponent,
     NotfoundComponent,
-    SearchCompComponent
+    SearchCompComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule

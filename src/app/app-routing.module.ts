6437c3e3743feafb2e61f5c3f068e0f08ee8506a
saretoduncan <saretoduncan@gmail.com>
2021-07-26 14:27:50 +0300

    add style to main css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
 
+import { RouterModule, Routes } from "@angular/router";
+import { SearchCompComponent } from "./search-comp/search-comp.component";
+import { RepositoriesComponent } from "./repositories/repositories.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
 
+const routes: Routes = [
+  { path: "search", component: SearchCompComponent },
+  { path: "repos", component: RepositoriesComponent },
+  { path: "", redirectTo: "/search", pathMatch: "full" },
+  { path: "err", component: NotfoundComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { GitSearchService } from "../services/git-search.service";
+import { GitRepoService } from "../services/git-repo.service";
+import { User } from "../models/user";
 
 @Component({
-  selector: 'app-repositories',
-  templateUrl: './repositories.component.html',
-  styleUrls: ['./repositories.component.css']
+  selector: "app-repositories",
+  templateUrl: "./repositories.component.html",
+  styleUrls: ["./repositories.component.css"],
 })
 export class RepositoriesComponent implements OnInit {
-
-  constructor() { }
+  userProfile!: User;
+  rep: any[] = [];
+  constructor(
+    private gitService: GitSearchService,
+    private gitRepoService: GitRepoService
+  ) {}
 
   ngOnInit(): void {
+    this.userProfile = this.gitService.userProfile;
+    this.rep = this.gitRepoService.repoArr;
   }
-
 }

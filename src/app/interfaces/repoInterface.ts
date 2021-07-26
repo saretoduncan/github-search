85d1cd27f98064fb56ee65e3eb4e8bde946b03ec
saretoduncan <saretoduncan@gmail.com>
2021-07-26 13:52:31 +0300

    add interface in user and repo interfaces

---
+++ src/app/interfaces/repoInterface.ts
@@ -0,0 +1,10 @@
+export interface Irepo {
+  name: string;
+  html_url: string;
+  description: string;
+  created_at: string;
+  clone_url: string;
+  homepage: string;
+  forks_count: number;
+  stargazers_count: number;
+}
---
+++ src/app/interfaces/userInterface.ts
@@ -0,0 +1,13 @@
+export interface IuserProfile {
+  login: string;
+  name: string;
+  avatar_url: string;
+  html_url: string;
+  repos_url: string;
+  followers: number;
+  following: number;
+  bio: string;
+  twitter_username: string;
+  public_repos: number;
+  location: string;
+}

34a410983a38176701ae2e068f8c08085ce69409
saretoduncan <saretoduncan@gmail.com>
2021-07-26 13:20:51 +0300

    add search-comp, repository component and notfound component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { SearchCompComponent } from './search-comp/search-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoriesComponent,
+    NotfoundComponent,
+    SearchCompComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/search-comp/search-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-comp',
+  templateUrl: './search-comp.component.html',
+  styleUrls: ['./search-comp.component.css']
+})
+export class SearchCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
